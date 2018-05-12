480e35a2d83ab311fba7e404fd816bdb575e6cf5
lokeshsam55 <lokeshsam555@gmail.com>
2018-05-12 23:51:11 +0530

    Streaming file with less time interval

---
+++ Streaming_data.py
@@ -0,0 +1,41 @@
+from __future__ import print_function
+from pyspark import SparkContext , SparkConf
+from pyspark.streaming import StreamingContext
+from pyspark.streaming.kafka import KafkaUtils
+import time
+from datetime import datetime
+import json
+import argparse
+
+def load_msg(msg):
+    message = json.loads(msg[1])
+    return message['remote_host'], 1
+
+def update_frequency(new_entry, freq_sum):
+  if not freq_sum:
+    freq_sum = 0
+  return sum(new_entry) + freq_sum
+
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser()
+  parser.add_argument('-rh', '--host', default="127.0.0.1:9092")
+  parser.add_argument('-t', '--topic', default='messages')
+  args = parser.parse_args()
+  print('Starting the process...\n')
+  start=datetime.now()
+  conf = SparkConf().setAppName("DetectingDDOS").setMaster("local")
+  sc = SparkContext(conf=conf)
+  ssc = StreamingContext(sc, 10)
+  ssc.checkpoint('chk')
+  conf.set("spark.streaming.receiver.writeAheadLog.enable", "true")
+  kvs = KafkaUtils.createDirectStream(ssc, [args.topic], {"metadata.broker.list": args.host})
+  parsed = kvs.map(load_msg)  
+  updated = parsed.updateStateByKey(update_frequency)
+  updated2 = updated.map(lambda (k,v): (str(k), v))
+  high_freq = updated2.filter(lambda (k,v): v >= 50)
+  high_freq.pprint()
+  high_freq.saveAsTextFiles('DDOS_Attacker__Output')
+  ssc.start()
+  time.sleep(60)
+  ssc.stop()
+  print('Process ended.')
